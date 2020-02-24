a96aaad0ddb89a1aa5be4b41a92630319f5cdd56
amlparkinson <aparkinson@ucsb.edu>
2020-02-24 12:25:14 -0800

    renamed R script to name of the function, set seed

--- global_dNNdt.R
+++ global_dNNdt.R
@@ -1,6 +1,8 @@
-# Assignment 1 - Hannah Garcia-Wickstrum & Anne-Marie Parkinson
+## Assignment 1 - Hannah Garcia-Wickstrum & Anne-Marie Parkinson 
 
 # This function determines global per capita growth rate based on average years of education completed, average daily kilocalorie intake per capita, HIV percent prevalence, and the gross domestic product.
+
+## function variables-----------------------------------------------------------------------------
 
 # 'intercept' was derived from a multiple linear regression model (see below)
 # 'education' coefficient was derived from a multple linear regression model, units = years (see below)
@@ -12,20 +14,38 @@
 # 'hiv_variable' is the percent prevalence of the disease in people ages 15 - 49, units = %
 # 'gdp_variable' is the global dollar amount of per capita gdp, units = USD ($)
 
+## load packages ---------------------------------------------------------------------------------
+
 library(tidyverse)
 library(dplyr)
 
-global_dNNdt2 = function(education_variable, kcal_variable, hiv_variable, gdp_variable, intercept=0.014, education=-0.001104, kcal=-0.000000004785, hiv=0.0005353, gdp =0.0000001285) {
+## linear regression model used to get coefficients ---------------------------------------------
+
+#lm(global_dnndt ~ education + kcal + hiv + gdp, data = population_data)
+
+## function to estimate global dN/Ndt------------------------------------------------------------
+
+global_dNNdt = function(education_variable, kcal_variable, hiv_variable, gdp_variable, 
+                        intercept=0.014, education=-0.001104, kcal=-0.000000004785, hiv=0.0005353, gdp =0.0000001285) {
+  
   # set parameters for education and kilocalories
-  education_variable = ifelse((education_variable < 0), return("education_variable cannot be less than zero"), education_variable) 
+  if (education_variable < 0) return("education_variable cannot be less than zero") 
   if (education_variable > 30) return("education_variable cannot be > 30")
-  kcal_variable = ifelse((kcal_variable < 0), NA, kcal_variable)
+  
+  if (kcal_variable < 0) return("kcal_variable cannot be < 0")
   if (kcal_variable > 25000) return("kcal_variable cannot be > 25000")
+  
   if (hiv_variable < 0) return("hiv_variable cannot be < 0")
-  hiv_variable = ifelse((hiv_variable > 100), return("hiv_variable cannot be > 100"), hiv_variable)
+  if (hiv_variable > 100) return("hiv_variable cannot be > 100")
+  
   if (gdp_variable < 0) return("gdp_variable cannot be < 0 ")
+  
+  # equation
   result = intercept + (education*education_variable) + (kcal*kcal_variable) + (gdp*gdp_variable) + (hiv*hiv_variable)
+  
   return(result)
 } 
 
-global_dNNdt2(10, 12000, 5, 7000)
+## test function ------------------------------------------------------------------------------------
+#global_dNNdt(-1, 11404.616, 10.3004753, 570.7973)
+
